7096491972c33b81b20e2f2641721f93640b160a
Waqar Muneer <i.do.dev@outlook.com>
2023-10-30 16:10:44 +0500

    update config file for AWS config constants

--- src/config-sample.ts
+++ src/config-sample.ts
@@ -3,3 +3,13 @@ export const POSTGRESQL_PORT = 5432;
 export const POSTGRESQL_USER = 'postgres';
 export const POSTGRESQL_PASSWORD = 'waqar';
 export const LOCAL_POSTGRESQL_DATABASE = 'c2e-services';
+
+export const AWS_CREDENTIALS = {
+  accessKeyId: 'YOUR_ACCESS_KEY_ID',
+  secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+}
+export const AWS_REGION = "";
+export const AWS_S3_BUCKET = "";
+export const AWS_S3_BUCKET_URL = "";
+export const MASTER_KEY = "";
+export const TEMP_FOLDER = "";
